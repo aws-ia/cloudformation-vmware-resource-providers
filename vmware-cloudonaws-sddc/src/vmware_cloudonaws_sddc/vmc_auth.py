bcf04a7809cce28bc6df3be969f6aa9ae07a65fd
Patrick Kremer <kremerpt@amazon.com>
2023-05-02 16:55:40 -0500

    Ran black code formatter
    Signed-off-by: Patrick Kremer <kremerpt@amazon.com>

--- vmware-cloudonaws-sddc/src/vmware_cloudonaws_sddc/vmc_auth.py
+++ vmware-cloudonaws-sddc/src/vmware_cloudonaws_sddc/vmc_auth.py
@@ -9,6 +9,7 @@ import json
 import requests
 import datetime
 
+
 class VMCAuth:
     def __init__(self, strCSPProdURL: str):
         self.access_token = None
@@ -16,20 +17,26 @@ class VMCAuth:
         self.activeRefreshToken = None
         self.strCSPProdURL = strCSPProdURL
 
-    def getAccessToken(self,myRefreshToken):
-        """ Gets the Access Token using the Refresh Token """
+    def getAccessToken(self, myRefreshToken):
+        """Gets the Access Token using the Refresh Token"""
         self.activeRefreshToken = myRefreshToken
-        params = {'api_token': myRefreshToken}
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        params = {"api_token": myRefreshToken}
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         try:
-            response = requests.post(f'{self.strCSPProdURL}/csp/gateway/am/api/auth/api-tokens/authorize', params=params, headers=headers)
+            response = requests.post(
+                f"{self.strCSPProdURL}/csp/gateway/am/api/auth/api-tokens/authorize",
+                params=params,
+                headers=headers,
+            )
             jsonResponse = response.json()
-            self.access_token = jsonResponse['access_token']
-            expires_in = jsonResponse['expires_in']
-            expirestime = datetime.datetime.now() + datetime.timedelta(seconds=expires_in)
+            self.access_token = jsonResponse["access_token"]
+            expires_in = jsonResponse["expires_in"]
+            expirestime = datetime.datetime.now() + datetime.timedelta(
+                seconds=expires_in
+            )
             self.access_token_expiration = expirestime
-            print(f'Token expires at {expirestime}')
+            print(f"Token expires at {expirestime}")
         except:
             self.access_token = None
             self.access_token_expiration = None
@@ -40,5 +47,5 @@ class VMCAuth:
         if self.access_token_expiration is not None:
             time_to_expire = self.access_token_expiration - datetime.datetime.now()
             if time_to_expire.total_seconds() <= 100:
-                print('Access token expired, attempting to refresh...')
-                self.getAccessToken(self.activeRefreshToken)        
+                print("Access token expired, attempting to refresh...")
+                self.getAccessToken(self.activeRefreshToken)
